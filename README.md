deee01929969db53aa21226b1ab6f936d4e9b80d
Robert Long <robert@robertlong.me>
2020-08-01 00:26:00 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# ecsy-three Webpack Boilerplate
+
+This is a simple boilerplate project for getting up and running with [ecsy-three](https://three.ecsy.io) and [Webpack](https://webpack.js.org/).
+
+## Getting Started
+
+Clone the repository and run:
+
+```
+npm install
+```
+
+To start the webpack-dev-server run:
+
+```
+npm start
+```
+
+To build the production ready bundle run:
+
+```
+npm run build
+```
+
+The bundled code will be output to the `/dist` directory.
+
+To deploy to Github Pages run:
+
+```
+npm run deploy
+```
+
+[View Live Demo](https://mixedreality.mozilla.org/ecsy-three-webpack/)
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "app.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new HTMLWebpackPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+  },
+};
